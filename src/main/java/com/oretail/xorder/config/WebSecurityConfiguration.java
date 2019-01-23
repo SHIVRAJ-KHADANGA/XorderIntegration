f711b695f4e5970e927ea33080562b39e4b7c53b
SHIVRAJ-KHADANGA <shivraj.khadanga@gmail.com>
2019-01-23 18:07:57 +0530

    Security fix and profiles activation - dev/prod

--- src/main/java/com/oretail/xorder/config/WebSecurityConfiguration.java
+++ src/main/java/com/oretail/xorder/config/WebSecurityConfiguration.java
@@ -1,16 +1,24 @@
 package com.oretail.xorder.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     //This is used for allowing post requests for resource creation without csrf token
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-    	http.csrf().disable();
-    }
+	/*
+	 * @Override public void configure(HttpSecurity http) throws Exception {
+	 * http.csrf().disable(); }
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests()
+				.anyRequest().authenticated()
+				.and().httpBasic();
+	}
 }
